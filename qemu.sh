4fc0cc3067100e75b58466a3e55cd1ed76916035
Daniel Schick <daniel.schick6@googlemail.com>
2018-11-04 15:29:55 +0100

    Makes the code runnable for my environment

--- qemu.sh
+++ qemu.sh
@@ -2,4 +2,6 @@
 set -e
 . ./iso.sh
 
-qemu-system-$(./target-triplet-to-arch.sh $HOST) -cdrom myos.iso
+#qemu-system-$(./target-triplet-to-arch.sh $HOST) -cdrom myos.iso
+qemu-system-x86_64 -cdrom myos.iso
+./clean.sh

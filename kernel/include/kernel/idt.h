879c4fc2a15b16a17e3b8a2d9f75ac02dd79f2a8
Daniel Schick <daniel.schick6@googlemail.com>
2018-11-10 18:20:08 +0100

    adds interrupt support

---
+++ kernel/include/kernel/idt.h
@@ -0,0 +1,8 @@
+#ifndef _KERNEL_IDT_H
+#define _KERNEL_IDT_H
+
+#include <stddef.h>
+
+void idt_init(void);
+
+#endif
--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -2,9 +2,12 @@
 
 #include <kernel/tty.h>
 #include <kernel/gtd.h>
+#include <kernel/idt.h>
 
 void kernel_main(void) {
 	initialiceGTD();
+	idt_init();
 	terminal_initialize();
 	printf("Hello, kernel World!\n");
+	while(1);
 }

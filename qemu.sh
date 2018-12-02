24e7824603665e6e5176b437f28ef990926aa616
Daniel Schick <daniel.schick6@googlemail.com>
2018-12-02 15:36:10 +0100

    Changes the Qemu-system in qemu.sh to i386(32bit)

--- qemu.sh
+++ qemu.sh
@@ -3,6 +3,6 @@ set -e
 . ./iso.sh
 
 #qemu-system-$(./target-triplet-to-arch.sh $HOST) -cdrom myos.iso
-qemu-system-x86_64 -cdrom myos.iso
+qemu-system-i386 -cdrom myos.iso
 #qemu-system-x86_64 -kernel kernel/myos.kernel
 ./clean.sh
